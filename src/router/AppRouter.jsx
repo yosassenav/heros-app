ffc51877ae03ba26c3ae728ad25580f19ce92590
yosassenav <mromerovanessa.soy@gmail.com>
2024-06-27 00:16:39 -0500

    adding routes to Navbar

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,17 +1,20 @@
-import { Navigate, Routes } from "react-router-dom"
-import { Marvel } from "../heroes/pages/Marvel"
-import { DC } from "../heroes/pages/DC"
-import { Login } from "../auth/pages/Login"
+import React from 'react';
+import { Navigate, Routes, Route } from "react-router-dom";
+import { Marvel } from "../heroes/pages/Marvel";
+import { DC } from "../heroes/pages/DC";
+import { Login } from "../auth/pages/Login";
+import { Navbar } from "../ui";
 
 export const AppRouter = () => {
     return (
         <>
+            <Navbar />
             <Routes>
-                <Route path="marvel" element={Marvel}/>
-                <Route path="dc" element={DC}/>
-                <Route path="login" element={Login}/>
-                <Route path="/" element={<Navigate to={"/marvel"}/>}/>
+                <Route path="marvel" element={<Marvel />} />
+                <Route path="dc" element={<DC />} />
+                <Route path="login" element={<Login />} />
+                <Route path="/" element={<Navigate to="/marvel" />} />
             </Routes>
         </>
-    )
-}
+    );
+};
