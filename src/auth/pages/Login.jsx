5ce0ac0bf4f5b903cb51ffbc8fa77e09cd8a1b81
yosassenav <mromerovanessa.soy@gmail.com>
2024-07-01 23:39:43 -0500

    adding Login component

--- src/auth/pages/Login.jsx
+++ src/auth/pages/Login.jsx
@@ -1,3 +1,24 @@
+import { useNavigate } from "react-router-dom"
+
 export const Login = () => {
-    return <h1>Login</h1>
+
+    const navigate = useNavigate()
+    const onLogin = ()=>{
+        navigate('/home', 
+            {
+                replace: true
+            })
+    }
+    return (
+        <div className="container mt-5">
+            <h1>Login</h1>
+            <hr/>
+            <button
+            className="btn btn-primary"
+            onClick={onLogin}
+            >
+                Login
+            </button>
+        </div>
+    )
 }
