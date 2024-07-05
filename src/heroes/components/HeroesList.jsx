20dd19a6b8aee471ef25628db26970e2dc1f0cae
yosassenav <mromerovanessa.soy@gmail.com>
2024-07-05 00:41:01 -0500

    fixing scafolding and adding new component HeroCard

--- src/heroes/components/HeroesList.jsx
+++ src/heroes/components/HeroesList.jsx
@@ -1,16 +1,20 @@
-import { getHeroesByPublisher } from "../helpers/"
+import { getHeroesByPublisher } from "../helpers"
+import { HeroCard } from "./HeroCard";
 
 export const HeroesList = ({publisher}) => {
 
     const heroes = getHeroesByPublisher(publisher);
 
     return (
-        <ul>
+        <div className="row row-cols-1 row-cols-md-3 g-3">
             {
                 heroes.map(hero => (
-                    <li key={hero.id}>{hero.superhero}</li>
+                    <HeroCard
+                    key={hero.id}
+                    {...hero}
+                    />
                 ))
             }
-        </ul>
+        </div>
     )
 }
