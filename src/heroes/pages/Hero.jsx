c650f4ac12f205cfde1faccecf9eb493c76cd519
yosassenav <mromerovanessa.soy@gmail.com>
2024-07-16 22:52:51 -0500

    adding a navigate route when the url provided does not exist

--- src/heroes/pages/Hero.jsx
+++ src/heroes/pages/Hero.jsx
@@ -1,13 +1,16 @@
-import { useParams } from "react-router-dom"
+import { Navigate, useParams } from "react-router-dom"
 import { getHeroById } from "../helpers";
 
 export const Hero = () => {
 
     const { id } = useParams();
 
-    const hero = getHeroById( id )
+    const hero = getHeroById( id );
 
-    console.log( hero )
+    if ( !hero ) {
+        return <Navigate to={"/marvel"}/>
+    }
 
-    return <h1>Hero</h1>
+    
+    return <h1>{hero.superhero}</h1>
 }
