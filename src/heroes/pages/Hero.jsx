b5a78eaf2a8aeb3dc5b515c74d091021f60741f2
yosassenav <mromerovanessa.soy@gmail.com>
2024-07-16 23:35:43 -0500

    adding onNavigateBack function to navigate bbackwards

--- src/heroes/pages/Hero.jsx
+++ src/heroes/pages/Hero.jsx
@@ -1,7 +1,9 @@
-import { Navigate, useParams } from "react-router-dom"
+import { Navigate, useNavigate, useParams } from "react-router-dom"
 import { getHeroById } from "../helpers";
 
 export const Hero = () => {
+
+    const navigate = useNavigate();
 
     const { id } = useParams();
 
@@ -11,6 +13,47 @@ export const Hero = () => {
         return <Navigate to={"/marvel"}/>
     }
 
-    
-    return <h1>{hero.superhero}</h1>
+    const onNavigateBack = () => {
+        navigate('/marvel',{
+            replace: true
+        })
+    }
+
+
+    return (
+        <div className="row mt-5">
+            <div className="col-4">
+                <img
+                src={`/assets/heroes/${ id }.jpg`}
+                alt={ hero.superhero }
+                className="img-thumbnail"
+                />
+            </div>
+            <div className="col-8">
+                <h3>{ hero.superhero }</h3>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item">
+                        <b>Alter ego:</b> { hero.alter_ego }
+                    </li>
+                    <li className="list-group-item">
+                        <b>Publisher:</b> { hero.publisher }
+                    </li>
+                    <li className="list-group-item">
+                        <b>First appearance:</b> { hero.first_appearance }
+                    </li>
+                </ul>
+                <h5 className="mt-3">
+                    Characters
+                </h5>
+                <p>{ hero.characters }</p>
+
+                <button
+                className="btn btn-outline-primary"
+                onClick={onNavigateBack}
+                >
+                    Back
+                </button>
+            </div>
+        </div>
+    )
 }
