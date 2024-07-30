e5788724c75ba72242e1b475c04a3bbcb4f1a9cc
yosassenav <mromerovanessa.soy@gmail.com>
2024-07-30 18:09:18 -0500

    adding an authReducer function + AuthProvider and AuthContext components + modifications in Login and Navbar components

--- src/auth/context/authReducer.js
+++ src/auth/context/authReducer.js
@@ -1,18 +1,21 @@
 import { types } from "../types/types";
 
-const initialState = {
-    logged: false,
-}
 
-export const authReducer = (action, state = initialState) => {
+export const authReducer = ( action, state = {} ) => {
 
     switch( action.type ){
 
         case types.login:
-            return state;
+            return {
+                ...state,
+                logged: true,
+                user: action.payload,
+            }
 
         case types.logout:
-            return state;
+            return {
+                logged: false,
+            }
 
         default:
             return state;
--- src/auth/pages/Login.jsx
+++ src/auth/pages/Login.jsx
@@ -1,9 +1,17 @@
+import { useContext } from "react"
 import { useNavigate } from "react-router-dom"
+import { AuthContext } from "../context/AuthContext"
 
-export const Login = () => {
+export const LoginPage = () => {
 
-    const navigate = useNavigate()
+    const { login } = useContext( AuthContext );
+
+    const navigate = useNavigate();
+
     const onLogin = ()=>{
+
+        login('Vanessa');
+
         navigate('/home', 
             {
                 replace: true
