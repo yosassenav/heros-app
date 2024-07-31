34ec588297421363fe04c9630e1e805beee94f17
yosassenav <mromerovanessa.soy@gmail.com>
2024-07-30 22:35:33 -0500

    adding implementation for private routes using react-router-dom

---
+++ src/router/PrivateRoutes.jsx
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react'
+import { Navigate, useLocation } from 'react-router-dom'
+import { AuthContext } from '../auth/context/AuthContext'
+
+export const PrivateRoutes = ({ children }) => {
+
+    const { isLogged } = useContext(AuthContext);
+
+    const {pathname, search} = useLocation();
+
+    const lastPath = pathname + search;
+
+    localStorage.setItem('lastPath',lastPath);
+
+  return (isLogged) ? children : <Navigate to='/login'/>
+}
