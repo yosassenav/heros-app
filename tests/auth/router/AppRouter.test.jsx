e5843ba98fa6b23ec59ddd26e8e56554a947c9f3
yosassenav <mromerovanessa.soy@gmail.com>
2024-09-15 23:57:43 -0500

    making modifications to AppRouter.test.jsx file

--- tests/auth/router/AppRouter.test.jsx
+++ tests/auth/router/AppRouter.test.jsx
@@ -1,22 +1,52 @@
-import { render } from "@testing-library/react"
+/**
+ * @jest-environment jsdom
+ */
+
+import React from "react"
 import { MemoryRouter } from "react-router-dom"
-import { AuthContext } from "../../../src/auth/context/AuthContext";
-import { AppRouter } from "../../../src/router/AppRouter";
+import { AppRouter } from "../../../src/router/AppRouter"
+import { AuthContext } from "../../../src/auth/context/AuthContext"
+import { render } from "@testing-library/react"
+
+
 
 describe('Pruebas en <AppRouter/>',()=>{
 
-    test('debe mostrar el login si NO esta autenticado',()=>{
+    // test('debe mostrar el login si NO esta autenticado',()=>{
 
+    //     const contextValue = {
+    //         isLogged: false,
+    //     }
+
+    //     render(
+    //         <MemoryRouter initialEntries={['/MarvelPage']}>
+    //             <AuthContext.Provider value={contextValue}>
+    //                 <AppRouter/>
+    //             </AuthContext.Provider>
+    //         </MemoryRouter> 
+    //     )
+
+    //     expect(screen.getAllByText()).toBeTruthy();
+
+    // })
+
+    test('debe mostrar el componente de Marvel si esta autenticado',()=>{
         const contextValue = {
-            isLogged: false
+            isLogged: true,
+            user: {
+                id: '123',
+                name: 'Michael'
+            }
         }
+
         render(
-            <MemoryRouter initialEntries={'/MarvelPage'}>
+            <MemoryRouter initialEntries={['/MarvelPage']}>
                 <AuthContext.Provider value={contextValue}>
-                    <AppRouter />
+                    <AppRouter/>
                 </AuthContext.Provider>
             </MemoryRouter>
-            
         )
+
+        expect(screen.getAllByText('Marvel')).toBeTruthy()
     })
 })
