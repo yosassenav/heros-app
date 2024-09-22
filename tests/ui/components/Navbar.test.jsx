5859af9106c34b5dbaa8dd8c7485c98c5604b148
yosassenav <mromerovanessa.soy@gmail.com>
2024-09-21 23:45:06 -0500

    finishing Navbar.test.jsx second test implementation

--- tests/ui/components/Navbar.test.jsx
+++ tests/ui/components/Navbar.test.jsx
@@ -3,10 +3,19 @@
  */
 
 import React from "react"
-import { render, screen } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import { Navbar } from "../../../src/ui/components/Navbar"
 import { AuthContext } from "../../../src/auth/context/AuthContext"
 import { MemoryRouter } from "react-router-dom"
+
+// mock of useNavigate hook from react testing library
+const mockedUseNavigate = jest.fn();
+
+jest.mock('react-router-dom',()=>({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: ()=>mockedUseNavigate
+}))
+
 
 describe('Puebas en <Navbar/>',()=>{
     const contextValue = {
@@ -32,10 +41,22 @@ describe('Puebas en <Navbar/>',()=>{
             
         );
 
-        screen.debug()
+        expect(screen.getByText('Juan')).toBeTruthy()
     })
 
     test('debe de llamar el logout y el navigate cuando se hace click en el boton de logout',()=>{
+        render(
+            <AuthContext.Provider value={contextValue}>
+                <MemoryRouter>
+                    <Navbar />
+                </MemoryRouter>
+            </AuthContext.Provider>
+            
+        );
 
+        const onlogoutBtn = screen.getByRole('button');
+        fireEvent.click(onlogoutBtn);
+        expect(contextValue.logout).toHaveBeenCalledTimes(1);
+        expect(mockedUseNavigate).toHaveBeenNthCalledWith('/login', {'replace': true})
     })
 })
